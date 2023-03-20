e5599897c77756198e82597c2eac64b69cb56710
Alex Bishop <alexb98@gmail.com>
2023-03-20 11:57:14 -0500

    Original structure created. Mob coded.

---
+++ src/scripts/tandf.js
@@ -0,0 +1,33 @@
+
+
+
+export const TruncheonAndFlagons = () => {
+    
+    
+    
+    let html = `
+    <nav id="selectionOptions">
+    <div id="newGame">
+    </div>
+    <div id="rosters">
+    </div>
+    <div id="pastGames">
+    </div>
+</nav>
+
+<main id="contentBody">
+    <aside id="leftColumn">
+        <div id="addTeam">
+        </div>
+        <div id="addPlayer">
+        </div>
+        <div id="leaderboard">
+        </div>
+    </aside>
+    <section id="renderedData">
+    </section>
+</main>`
+
+
+return html
+}
